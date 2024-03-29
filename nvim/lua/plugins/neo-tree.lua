8202f550e4ee3580e9363b42bebf82b751aab871
Stuart Thompson <stuart@stuartthompson.net>
2024-03-29 16:30:49 -0700

    Additional neotree configuration.

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,7 @@
+-- NeoTree Configuration
+--
+-- This plugin provides a file tree browser as a side bar.
+
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -5,5 +9,12 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim"
+    },
+    -- :Neotree action=show source=buffers position=right toggle=true
+    keys = {
+        { "<Leader>tt", "<Cmd>Neotree action=focus source=filesystem<CR>", desc = "Show file tree" },
+        { "<Leader>tb", "<Cmd>Neotree action=focus source=buffers<CR>", desc = "Show buffer tree" },
+        { "<Leader>tf", "<Cmd>Neotree action=focus<CR>", desc = "Focus file tree" },
+        { "<Leader>tc", "<Cmd>Neotree action=close<CR>", desc = "Close file tree" },
     }
 }
