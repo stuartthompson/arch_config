d4b99f2a468679ee310e7381c0b1ff979b815c31
Stuart Thompson <stuart@stuartthompson.net>
2024-03-29 13:47:31 -0700

    Introduces lualine plugin.

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,63 @@
+-- vim: set tabstop=2 softtabstop=2 shiftwidth=2:
+
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+  end,
+  opts = function()
+    local colors = require('stu.colors')
+    return {
+      options = {
+        icons_enabled = true,
+        theme = {
+          normal = { 
+            a = { fg = colors.gray.lightest, bg = colors.gray.normal } ,
+            b = { fg = colors.gray.lightest, bg = colors.gray.darker },
+            c = { fg = colors.gray.lightest, bg = colors.gray.darker },
+            x = { fg = colors.gray.lightest, bg = colors.gray.darker },
+            y = { fg = colors.gray.lightest, bg = colors.gray.darker },
+            z = { fg = colors.gray.lightest, bg = colors.gray.darker },
+          },
+          insert = {
+            a = { fg = colors.green.lightest, bg = colors.green.darker },
+          },
+          visual = {
+            a = { fg = colors.purple.lightest, bg = colors.purple.darker },
+          },
+          replace = {
+            a = { fg = colors.orange.lightest, bg = colors.orange.darker },
+          }
+        },
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
+    }
+  end
+}
