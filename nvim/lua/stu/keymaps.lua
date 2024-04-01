89b75eef8411ad31f53fcb8a3708b655a22153eb
Stuart Thompson <stuart@stuartthompson.net>
2024-04-01 14:25:24 -0700

    Adds Bufferline keymaps.

--- nvim/lua/stu/keymaps.lua
+++ nvim/lua/stu/keymaps.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_set_keymap('n', '<Leader>i', ':Inspect<CR>', { noremap = true, silent = true })
 
 local wk = require("which-key")
+local ts = require("telescope.builtin")
 local gs = require("gitsigns")
 
 -- Raw keymaps
@@ -8,28 +9,56 @@ wk.register({
     -- Previous
     ["["] = {
         name = "Previous",
-        h = { "<cmd>GitSigns prev_hunk<cr>", "Previous Hunk" },
+        b = { "<cmd>BufferLineCyclePrev<cr>", "Previous buffer (tab)" },
+        h = { "<cmd>GitSigns prev_hunk<cr>", "Previous hunk" },
     },
     -- Next
     ["]"] = {
         name = "Next",
-        h = { "<cmd>Gitsigns next_hunk<cr>", "Next Hunk" },
+        b = { "<cmd>BufferLineCycleNext<cr>", "Next buffer (tab)" },
+        h = { "<cmd>Gitsigns next_hunk<cr>", "Next hunk" },
     },
 })
 
 -- Leader keymaps
 wk.register({
+    -- Bufferline (Tabs)
+    b = {
+        name = "Buffers (Tabs)",
+        p = { "<cmd>BufferLineTogglePin<cr>", "Toggle buffer pin" },
+        o = { "<cmd>BufferLineCloseOthers<cr>", "Close other buffers" },
+        r = { "<cmd>BufferLineCloseRight<cr>", "Close buffers to right" },
+        l = { "<cmd>BufferLineCloseLeft<cr>", "Close buffers to left" },
+        ["["] = { "<cmd>BufferLineMovePrev<cr>", "Move buffer left" },
+        ["]"] = { "<cmd>BufferLineMoveNext<cr>", "Move buffer right" },
+        c = { "<cmd>bd<cr>", "Close current buffer" },
+
+    },
+    -- Telescope (Find Files)
+    f = {
+        name = "Find Files",
+        f = { ts.find_files, "Find files" },
+        g = { ts.live_grep, "Live grep files" },
+        b = { ts.buffers, "Find buffers" },
+        h = { ts.help_tags, "Find help" },
+        i = {
+            name = "Find ignored files",
+            f = { function() ts.find_files({ no_ignore = true }) end, "Find files (incl. ignored)" },
+            g = { function() ts.live_grep({ no_ignore = true }) end, "Live grep (incl. ignored)" },
+        }
+    },
     -- Git
     g = {
         name = "Git",
-        s = { "<cmd>Gitsigns stage_hunk<cr>", "Stage Hunk" },
-        S = { "<cmd>Gitsigns stage_buffer<cr>", "Stage Buffer" },
-        r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset Hunk" },
-        R = { "<cmd>Gitsigns reset_buffer<cr>", "Reset Buffer" },
-        u = { "<cmd>Gitsigns undo_stage_hunk<cr>", "Undo Stage Hunk" },
-        p = { "<cmd>Gitsigns preview_hunk_inline<cr>", "Preview Hunk Inline" },
-        b = { function() gs.blame_line({ full = true }) end, "Blame Line" },
-        d = { "<cmd>Gitsigns diffthis<cr>", "Diff This" },
-        D = { function() gs.diffthis("~") end, "Diff This ~" },
-    }
+        s = { "<cmd>Gitsigns stage_hunk<cr>", "Stage hunk" },
+        S = { "<cmd>Gitsigns stage_buffer<cr>", "Stage buffer" },
+        r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset hunk" },
+        R = { "<cmd>Gitsigns reset_buffer<cr>", "Reset buffer" },
+        u = { "<cmd>Gitsigns undo_stage_hunk<cr>", "Undo stage hunk" },
+        p = { "<cmd>Gitsigns preview_hunk_inline<cr>", "Preview hunk inline" },
+        b = { function() gs.blame_line({ full = true }) end, "Blame line" },
+        d = { "<cmd>Gitsigns diffthis<cr>", "Diff this" },
+        D = { function() gs.diffthis("~") end, "Diff this ~" },
+    },
 }, { prefix = "<leader>" })
+
