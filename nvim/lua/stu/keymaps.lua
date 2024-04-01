990b633fd43c6fd6b31e371764359724b90a5df0
Stuart Thompson <stuart@stuartthompson.net>
2024-04-01 14:09:02 -0700

    Cleaning up git keymaps. Learning about whichkey groups.

--- nvim/lua/stu/keymaps.lua
+++ nvim/lua/stu/keymaps.lua
@@ -1,26 +1,35 @@
 vim.api.nvim_set_keymap('n', '<Leader>i', ':Inspect<CR>', { noremap = true, silent = true })
 
 local wk = require("which-key")
+local gs = require("gitsigns")
+
+-- Raw keymaps
 wk.register({
+    -- Previous
+    ["["] = {
+        name = "Previous",
+        h = { "<cmd>GitSigns prev_hunk<cr>", "Previous Hunk" },
+    },
+    -- Next
+    ["]"] = {
+        name = "Next",
+        h = { "<cmd>Gitsigns next_hunk<cr>", "Next Hunk" },
+    },
+})
+
+-- Leader keymaps
+wk.register({
+    -- Git
     g = {
         name = "Git",
         s = { "<cmd>Gitsigns stage_hunk<cr>", "Stage Hunk" },
+        S = { "<cmd>Gitsigns stage_buffer<cr>", "Stage Buffer" },
         r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset Hunk" },
+        R = { "<cmd>Gitsigns reset_buffer<cr>", "Reset Buffer" },
+        u = { "<cmd>Gitsigns undo_stage_hunk<cr>", "Undo Stage Hunk" },
+        p = { "<cmd>Gitsigns preview_hunk_inline<cr>", "Preview Hunk Inline" },
+        b = { function() gs.blame_line({ full = true }) end, "Blame Line" },
+        d = { "<cmd>Gitsigns diffthis<cr>", "Diff This" },
+        D = { function() gs.diffthis("~") end, "Diff This ~" },
     }
 }, { prefix = "<leader>" })
-      -- Hunk navigation
-      --map("n", "]h", gs.next_hunk, "Next Hunk")
-      --map("n", "[h", gs.prev_hunk, "Prev Hunk")
-      ---- Stage and reset in normal and visual mode
-      --map({"n","v"}, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-      --map({"n","v"}, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      ---- Stage buffer, reset, undo
-      --map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
-      --map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
-      --map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
-      --map("n", "<leader>gp", gs.preview_hunk_inline, "Preview Hunk Inline")
-      --map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
-      ---- Diff
-      --map("n", "<leader>gd", gs.diffthis, "Diff This")
-      --map("n", "<leader>gD", function() gs.diffthis("~") end, "Diff This ~")
-
