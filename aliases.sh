8f0db9b0fc51c84b5ff0bb45de0e1088a74eda7c
Stuart Thompson <stuart@stuartthompson.net>
2024-04-12 11:52:04 -0700

    Separates aliases into their own file.

---
+++ aliases.sh
@@ -0,0 +1,7 @@
+# l - list folder contents (using eza)
+alias l='eza -al --icons=always'
+alias lt='eza -al --tree --icons=always'
+alias ltgi='exa -al --tree --git-ignore --icons=always'
+
+# c - cat file (using bat)
+alias c='bat'
